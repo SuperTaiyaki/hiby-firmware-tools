5ec043a5aa5072502ea98bd3131c4c94518f09d6
Jeremy Chin <git@jeremyc.f-m.fm>
2022-02-06 22:16:30 +0900

    Quoting problem

--- build_update.sh
+++ build_update.sh
@@ -4,6 +4,6 @@
 # If the update fails, try adding or removing some blank lines to match the original file.
 # Also this sed command to rewrite the update assumes the kernel section is in place
 
-sed -i '7,$s/md5=.*/md5='"(md5sum customfw/SYSTEM.UBI | cut -d\  -f1)/" customfw/UPDATE.TXT
+sed -i '7,$s/md5=.*/md5='"$(md5sum customfw/SYSTEM.UBI | cut -d\  -f1)/" customfw/UPDATE.TXT
 genisoimage -l -o r3pro_custom.upt customfw/{SYSTEM.UBI,UBOOT.BIN,UIMAGE.BIN,UPDATE.TXT,VERSION.TXT,_GITIGNO}
 
